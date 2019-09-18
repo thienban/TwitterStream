a666ce2c604c8d5127d3b126db92349eed240d9e
Thien Ban BUI <thienban91@hotmail.fr>
2019-09-18 17:33:14 +0200

    add style to card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
+import styled from "styled-components";
 
+const Container = styled.div`
+  border: 1px solid;
+  padding: 25px 12px 18px;
+  width: 20%;
+  height: 20%;
+  margin-top: 5px;
+  margin-bottom: 10px;
+`
+const StyledPhoto = styled.img`
+  margin: 0 5px;
+  border: 1px solid
+`
+
+const ActionButton = styled.button`
+  margin: 0 5px;
+  padding: 8px 14px;
+  background: grey;
+  color: black;
+  cursor: pointer;
+  border: 1px solid #fff;
+  outline: 0;
+  font-weight: 300;
+  :hover {
+    opacity: 0.8;
+  }
+`
+
+const Description = styled.p`
+  color: black;
+  font-weight: 300;
+`
 interface CardProps {
     key: number
     data: any
 }
 const CardComponent: React.FC<CardProps> = ({key, data}) => {
         return (
-            <div key={key}>
-                <div className="col s2">
-                    <img src={data.user.profile_image_url} alt={data.user.name} className="circle responsive-img" />
-                </div>
-                <div className="col s10 left-align">
-                    <span className="black-text">{data.text}</span>
-                </div>
-                <div className="row valign-wrapper right-align chip hoverable">
-                    {new Date(data.created_at).toLocaleTimeString()}
+            <Container key={key}>
+                <div>
+                    <StyledPhoto src={data.user.profile_image_url} alt={data.user.name} className="circle responsive-img" />
+                    <ActionButton>
+                        {new Date(data.created_at).toLocaleTimeString()}
+                    </ActionButton>
+                    <ActionButton>
+                    <a href={`https://twitter.com/${data.user.screen_name}`} target="_blank">{`@${data.user.screen_name}`}</a>
+                    </ActionButton>
                 </div>
                 <div>
-                    <a href={`https://twitter.com/${data.user.screen_name}`} target="_blank">{`@${data.user.screen_name}`}</a>
+                    <Description>{data.text}</Description>
                 </div>
-            </div>
+            </Container>
         );
 }
 
