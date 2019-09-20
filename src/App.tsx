d09e49acad187d2daaeea8274d6a61e1612bd1ce
Thien Ban BUI <thienban91@hotmail.fr>
2019-09-20 16:17:04 +0200

    refactor form to use Formik form

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,16 @@
-import React, {useState} from 'react';
-import {Grid2Columns, Grid1Columns} from './components/grid'
-import Input from './components/input';
-import Button from './components/button'
-import {useUserfrom} from './hooks/formHook';
-import List from './components/list';
+import React from 'react';
+import {Grid1Columns} from './components/grid'
 import styled from "styled-components";
+import Form from './components/form';
 
 const App: React.FC = () => {
-  const [reset, setReset] = useState(false);
+
   const Background = styled.div`
   background: linear-gradient(20deg, rgb(219, 112, 147), rgb(218, 163, 87));
   `;
-  const { inputs, handleInputChange } = useUserfrom({});
-  console.log(inputs);
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    const first = inputs ? inputs.input1:"";
-    const second = inputs ? inputs.input2:"";
-    const term = first+','+second;
-    fetch("http://localhost:3001/setSearchTerm",
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ term })
-      })
-    setReset(false);
-  }
-  const handleReset = (e: any) => {
-    e.preventDefault();
-    fetch("http://localhost:3001/pause",
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }).then(res => {
-        console.log(res);
-        setReset(true);
-      }) ;
-  }
   return (
     <Background>
-      <Grid2Columns>
-      <Input
-        Type="text"
-        Name="input1"
-        Input= {inputs ? inputs.input1 : undefined}
-        onChange= {handleInputChange}
-      />
-      <Input
-        Type="text"
-        Name="input2"
-        Input= {inputs ? inputs.input2 : undefined}
-        onChange= {handleInputChange}
-
-      />
-      <Button label="GO" onClick={handleSubmit}/>
-      <Button label="RESET" onClick={handleReset}/>
-      </Grid2Columns>
-      <Grid1Columns>
-        <List mustReset={reset}/>
-      </Grid1Columns>
+      <Form/>
       <Grid1Columns>
         <p>Graphique</p>
       </Grid1Columns>
---
+++ src/components/form.tsx
@@ -0,0 +1,70 @@
+import React,{useState} from 'react';
+import { Formik, Field, Form, FormikActions } from 'formik';
+import List from './list';
+import {Grid1Columns} from './grid'
+
+
+interface Values {
+    keyword1: string;
+    keyword2: string;
+  }
+const Inputs: React.FC<{}> = () => {
+    const [reset, setReset] = useState(false);
+    const handleReset = (e: any) => {
+        e.preventDefault();
+        fetch("http://localhost:3001/pause",
+          {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          }).then(res => {
+            console.log(res);
+            setReset(true);
+          }) ;
+      }
+    return (
+    <div className="container">
+      <h1>Stream Tweet API</h1>
+      <Formik
+        initialValues={{
+          keyword1: '',
+          keyword2: ''
+        }}
+        onSubmit={(values: Values, { setSubmitting }: FormikActions<Values>) => {
+          setTimeout(() => {
+            console.log(values);
+            setReset(false);
+            const term = values.keyword1+","+values.keyword2
+            fetch("http://localhost:3001/setSearchTerm",
+                {
+                    method: "POST",
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ term })
+                })
+            setSubmitting(false);
+          }, 500);
+        }}
+        render={() => (
+        <Form>
+            <Field type="text" name="keyword1" />
+            <Field type="text" name="keyword2" />
+            <button type="submit">
+              Submit
+            </button>
+            <button onClick={handleReset}>
+              reset
+            </button>
+        </Form>
+        )}
+      />
+      <Grid1Columns>
+        <List mustReset={reset}/>
+      </Grid1Columns>
+    </div>
+    );
+};
+
+export default Inputs;
